21c63583f075fa4dfd7b5ab5cf7b10bc703a7ad9
dbiswal <debasmitabiswal@gmail.com>
2015-10-06 15:37:25 -0600

    Created first draft of our script

--- extract.sh
+++ extract.sh
@@ -1,3 +1,7 @@
-Debasmita October 6th, 2015
+#Debasmita October 6th, 2015
 
-Kevin October 6th, 2015
+#Kevin October 6th, 2015
+
+#This script will extract unique elements from a column of a csv file
+
+cut -f 2 -d',' data/antibiotics.csv | sort | uniq > antibiotics_unique.csv
